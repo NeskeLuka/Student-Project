2758a9832e723b3f406b9660267ab8923d3d6b38
Neske <agentrambo63@gmail.com>
2026-03-21 15:03:28 +0100

    Uploading Tests part 1 for website

---
+++ Project/DbSetMocker.cs
@@ -0,0 +1,20 @@
+namespace Project.Tests
+{
+    using Microsoft.EntityFrameworkCore;
+    using Moq;
+
+    public static class DbSetMocker
+    {
+        public static Mock<DbSet<T>> Create<T>(IQueryable<T> data) where T : class
+        {
+            var mockSet = new Mock<DbSet<T>>();
+
+            mockSet.As<IQueryable<T>>().Setup(m => m.Provider).Returns(data.Provider);
+            mockSet.As<IQueryable<T>>().Setup(m => m.Expression).Returns(data.Expression);
+            mockSet.As<IQueryable<T>>().Setup(m => m.ElementType).Returns(data.ElementType);
+            mockSet.As<IQueryable<T>>().Setup(m => m.GetEnumerator()).Returns(() => data.GetEnumerator());
+
+            return mockSet;
+        }
+    }
+}
